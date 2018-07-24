2d1f8efeeddeb8238e71bb8a46b1ed69b456e685
AndreyIsakov <Andrey.Isakov@waveaccess.ru>
2018-07-24 18:13:35 +0300

    docker-new-2

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#@IgnoreInspection BashAddShebang
 sleep 8                     # грязный хак, чтобы приложение раньше базы не завелось
 PGPASSWORD=postgres  psql -h 0.0.0.0 -p 5422 -U postgres  -c "create database waves";
 java -jar waves-1.jar
